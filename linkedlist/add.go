35c0aa329e1806bb1aa690fa29ed8ca1b2023333
alexey-ernest <alexey.ernest@gmail.com>
2019-11-30 17:49:02 -0800

    Add forward order

--- linkedlist/add.go
+++ linkedlist/add.go
@@ -46,3 +46,65 @@ func AddReversed(l1, l2 *Node) *Node {
 
 	return res
 }
+
+func Add(l1, l2 *Node) *Node {
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	len1 := length(l1)
+	len2 := length(l2)
+	if len1 < len2 {
+		l1 = padd(l1, len2-len1)
+	} else if len2 < len1 {
+		l2 = padd(l2, len1-len2)
+	}
+
+	res, ovf := addEven(l1, l2)
+	if ovf > 0 {
+		n := &Node {
+			Value: ovf,
+			Next: res,
+		}
+		res = n
+	}
+	return res
+}
+
+func addEven(l1, l2 *Node) (*Node, int) {
+	if l1 == nil {
+		return nil, 0
+	}
+	res, ovf := addEven(l1.Next, l2.Next)
+	n := &Node {
+		Value: l1.Value + l2.Value + ovf,
+		Next: res,
+	}
+	if n.Value < 10 {
+		return n, 0
+	}
+	n.Value = n.Value % 10
+	return n, 1
+}
+
+func length(l *Node) int {
+	counter := 0
+	for p := l; p != nil; p = p.Next {
+		counter += 1
+	}
+	return counter
+}
+
+func padd(l *Node, k int) *Node {
+	for i := 0; i < k; i += 1 {
+		n := &Node {
+			Value: 0,
+			Next: l,
+		}
+		l = n
+	}
+	return l
+}
--- linkedlist/add_test.go
+++ linkedlist/add_test.go
@@ -177,3 +177,84 @@ func TestAddReversedNotEvenWithOverflow(t *testing.T) {
 		t.Errorf("addition failed")
 	}
 }
+
+func TestAddForwardOverflow(t *testing.T) {
+	l1 := &Node{
+		Value: 6,
+		Next: &Node{
+			Value: 1,
+			Next: &Node{
+				Value: 7,
+			},
+		},
+	}
+	l2 := &Node{
+		Value: 2,
+		Next: &Node{
+			Value: 9,
+			Next: &Node{
+				Value: 5,
+				Next: &Node{
+					Value: 3,
+				},
+			},
+		},
+	}
+	res := Add(l1, l2)
+	if res == nil {
+		t.Errorf("addition failed")
+		return
+	}
+
+	exp := [...]int{3, 5, 7, 0}
+	i := 0
+	for p := res; p != nil; p = p.Next {
+		if p.Value != exp[i] {
+			t.Errorf("%d != %d", p.Value, exp[i])
+			break
+		}
+		i++
+	}
+	if i < len(exp) {
+		t.Errorf("addition failed")
+	}
+}
+
+func TestAddForwardEven(t *testing.T) {
+	l1 := &Node{
+		Value: 6,
+		Next: &Node{
+			Value: 1,
+			Next: &Node{
+				Value: 7,
+			},
+		},
+	}
+	l2 := &Node{
+		Value: 2,
+		Next: &Node{
+			Value: 9,
+			Next: &Node{
+				Value: 5,
+			},
+		},
+	}
+	res := Add(l1, l2)
+	if res == nil {
+		t.Errorf("addition failed")
+		return
+	}
+
+	exp := [...]int{9, 1, 2}
+	i := 0
+	for p := res; p != nil; p = p.Next {
+		if p.Value != exp[i] {
+			t.Errorf("%d != %d", p.Value, exp[i])
+			break
+		}
+		i++
+	}
+	if i < len(exp) {
+		t.Errorf("addition failed")
+	}
+}
